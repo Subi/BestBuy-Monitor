a903d8b0d9151ce972c5f097d61523126b00f276
Subi <itsnegus@gmail.com>
2020-04-14 20:08:30 -0400

    removed senstitive information

--- src/format.js
+++ src/format.js
@@ -5,7 +5,7 @@ const stock = data => {
     const product = {
         Name: data.sku.names.short,
         Sku: data.sku.skuId,
-        Price: "$" + data.sku.price.currentPrice,
+        Price:  `$${data.sku.price.currentPrice}`,
         Image: `https://pisces.bbystatic.com/image2/BestBuy_US/images/products/${data.sku.skuId.substr(0,4)}/${data.sku.skuId}_sa.jpg;maxHeight=1000;maxWidth=1000`,
         Status: data.sku.buttonState.buttonState,
         Url: `https://bestbuy.com${data.sku.url}`
