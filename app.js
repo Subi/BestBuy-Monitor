48213093e9d86804f6bd0865036ad8a5545926e0
Subi <itsnegus@gmail.com>
2020-04-14 20:27:02 -0400

    improved error handling

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 const request =  require('./src/request');
 const config = require('./config/setting.json');
 const chalk = require('chalk');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/bestbuy' , {useNewUrlParser: true , useUnifiedTopology: true , useFindAndModify: false})
 .then(() => console.log(chalk.green("Mongo DB is connected")))
 .then(() => 
 (timer = () => {
-    console.log(chalk.blue("Monitor started"))
+    console.log(chalk.blue("Monitor is starting up!"))
     setInterval(() => {
-        console.log(chalk.blue("Monitoring.."))
-        config.Skus.forEach(async sku => {
-            await request.get(sku)
-        })
+        checkConfig()
     } , config.Interval)
 })())
 .catch(err => console.log(chalk.red(err)))
 
 
+const checkConfig = () =>  {
+    if(config.Discord.Webhook == ""){
+        console.log(chalk.red("Make sure to insert a discord webhook in the setting.json file"))
+        process.exit(1)
+    }
+    if(config.Skus.length != 1){
+        console.log(chalk.red("Make sure to insert a sku into the \" skus array [] \" "))
+        process.exit(1)
+    }
+    console.log(chalk.blue("Monitoring.."))
+    start()
+}
+
+
+const start = () => {
+    config.Skus.forEach(async sku => {
+        await request.get(sku)
+    })
+}
 
